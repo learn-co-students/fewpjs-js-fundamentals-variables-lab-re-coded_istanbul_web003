544a8b1b4a87daf64f613c43b94fc4a6d09a2c67
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-07-18 12:42:44 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = "Chelsea";
+let Chelsea = "Upper West Side";
+let companyCeo = "Lauren Hart";
+companyCeo = "Susan Smith";
